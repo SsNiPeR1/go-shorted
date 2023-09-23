14f0b70b4d8c809c3fab97dbd90d555bcde93aec
SsNiPeR1 <63201833+SsNiPeR1@users.noreply.github.com>
2023-09-23 20:07:08 +0200

    move API routes

--- functions/API/DBHandler.go
+++ functions/API/DBHandler.go
@@ -1,4 +1,4 @@
-package functions
+package api
 
 import (
 	"database/sql"
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/SsNiPeR1/go-shorted/config"
 	"github.com/SsNiPeR1/go-shorted/functions"
+	api "github.com/SsNiPeR1/go-shorted/functions/API"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -26,11 +27,12 @@ func main() {
 	r.GET("/", functions.IndexHandler)
 	r.GET("/static/:file", functions.StaticHandler)
 	r.GET("/:shorted", functions.RedirectHandler(db)) // :shorted is a parameter which can be accessed with c.Param("shorted")
+	r.POST("/shorten", functions.ShortenHandler(db))  // POST is needed to transfer data from the form to the server
 
-	r.POST("/db", functions.DBHandler(db)) // for debugging purposes only, can be used only by admins with the admin key POSTed
-	r.POST("/vanity", functions.VanityHandler(db))
-	r.POST("/delete", functions.DeleteHandler(db))
-	r.POST("/shorten", functions.ShortenHandler(db))
+	// API routes
+	r.POST("/api/v1/vanity", api.VanityHandler(db))
+	r.POST("/api/v1/db", api.DBHandler(db)) // for debugging purposes only, can be used only by admins with the admin key POSTed
+	r.POST("/api/v1/delete", api.DeleteHandler(db))
 
 	// log the time and date when the server starts, as well as the port it's listening on.
 	fmt.Printf("[SHORTED] %s - %s | \033[32mListening on port 8080\033[0;0m\n", time.Now().Format("2006/01/02"), time.Now().Format("15:04:05"))
