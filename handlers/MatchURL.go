20430aaff8aae13e2f6636e5c30ec4b9c3164a94
SsNiPeR1 <63201833+SsNiPeR1@users.noreply.github.com>
2023-10-31 09:08:53 +0100

    Update MatchURL.go
    support single-letter domain names

--- handlers/MatchURL.go
+++ handlers/MatchURL.go
@@ -5,7 +5,7 @@ import (
 )
 
 func MatchURL(url string) bool {
-	regex := `^((http(s)?:\/\/)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{1,24}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))$`
+	regex := `^((http(s)?:\/\/)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{1,24}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))$`
 
 	match, err := regexp.Match(regex, []byte(url))
 	if !match || err != nil {
